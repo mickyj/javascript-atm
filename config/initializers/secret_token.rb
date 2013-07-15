ff78f85375356ac443e881e40b96040779f19b7a
Mike <jarrellmike@gmail.com>
2013-07-16 00:12:54 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atm::Application.config.secret_token = '620aa758339fe08d5a9fe9ed4224617d8e6b6274cde23c3f836df1ee7165da7c16f469da71d2e4cd3555a2c955cb18b2b21379e2b4f0ff550efc63d9d8f674e4'
